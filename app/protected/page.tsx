e1915962e05ab73fcb334f9914f8809c1b73396e
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-09-27 04:48:45 +0200

    first commit21

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -15,7 +15,7 @@ export default async function ProtectedPage() {
       <>
 
 <div className=' '>
-<div className="  relative w-full bg-ethBlack-950 text-white h-screen font-bold flex flex-col max-w-xl">
+<div className="   w-full bg-ethBlack-950 text-white h-screen font-bold flex flex-col max-w-xl">
 <div className="px-4 z-10">
 
 
@@ -39,10 +39,11 @@ export default async function ProtectedPage() {
           </div>
 
     </div>
+    <div className='relative grow' ></div>
     </div>
     
 
-<div className=' absolute  top-5 rounded-t-lg bg-ethBlack-300 grow border-t-2  shadow-top-yellow  border-ethYellow-500 w-full '>
+<div className=' absolute  top-0 rounded-t-lg  bg-ethGray-500 grow border-t-2  shadow-top-yellow  border-ethYellow-500 w-full '>
 
 </div>
 
