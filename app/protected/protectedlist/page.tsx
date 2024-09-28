eadd79f52dcb2e076268fdac299272fb896e9745
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-09-29 00:36:45 +0200

    first commit75

--- app/protected/protectedlist/page.tsx
+++ app/protected/protectedlist/page.tsx
@@ -25,14 +25,14 @@ function Page() {
         </div>
       ))}
     </div>
-    <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs">
+    <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs p-4">
         <div className="text-center text-[#85827d] w-1/5 bg-[#1c1f24] m-1 p-2 rounded-2xl">
         {/*   <img src={binanceLogo} alt="Exchange" className="w-8 h-8 mx-auto" /> */}
-          <p className="mt-1">Exchange</p>
+          <p className="mt-1  text-lg  font-extrabold text-ethYellow-600">Mela</p>
         </div>
         <div className="text-center text-[#85827d] w-1/5">
           <Mine className="w-8 h-8 mx-auto" />
-          <p className="mt-1">Mine</p>
+          <p className="mt-1">Bank</p>
         </div>
         <div className="text-center text-[#85827d] w-1/5">
           <Friends className="w-8 h-8 mx-auto" />
@@ -42,10 +42,10 @@ function Page() {
           <Coins className="w-8 h-8 mx-auto" />
           <p className="mt-1">Earn</p>
         </div>
-        <div className="text-center text-[#85827d] w-1/5">
-      {/*     <img src={hamsterCoin} alt="Airdrop" className="w-8 h-8 mx-auto" /> */}
+        {/* <div className="text-center text-[#85827d] w-1/5">
+         <img src={hamsterCoin} alt="Airdrop" className="w-8 h-8 mx-auto" /> 
           <p className="mt-1">Airdrop</p>
-        </div>
+        </div> */}
       </div>
     </div>
   )
