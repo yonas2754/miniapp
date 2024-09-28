508400a63cc41cebb0568952aec86eab2b3ab1a4
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-09-28 22:39:01 +0200

    first commit60

--- app/protected/protectedlist/page.tsx
+++ app/protected/protectedlist/page.tsx
@@ -11,7 +11,7 @@ function Page() {
     <div>
       {/* Create an array with 100 elements and map over it */}
       {Array.from({ length: 100 }).map((_, index) => (
-        <div className="px-4 mt-4 fl grid-cols-3 gap-1  overflow-scroll " key={index}>
+        <div className="px-4 mt-4 grid grid-cols-3 gap-1  overflow-scroll " key={index}>
            <Link href="/protected/protectedlist">
           <div className="p-4 rounded-full circle-outer">
             <div className="w-full h-full rounded-full circle-inner">
