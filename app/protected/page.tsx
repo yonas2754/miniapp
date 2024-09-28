602bb8d5064d8da310586986c693e882a6373ff0
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-09-29 00:44:54 +0200

    first commit76

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -26,6 +26,9 @@ import {
   AvatarFallback,
   AvatarImage,
 } from "@/components/ui/avatar"
+import Mine from '@/components/icons/Mine';
+import Friends from '@/components/icons/Friends';
+import Coins from '@/components/icons/Coins';
 export default async function ProtectedPage() {
   const session = await getSession()
     return (
@@ -267,6 +270,31 @@ export default async function ProtectedPage() {
 </div>
 
 </div>
+
+
+
+<div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs p-2">
+        <div className="text-center text-[#85827d] w-1/5 bg-[#1c1f24] m-1 p-2 rounded-2xl">
+        {/*   <img src={binanceLogo} alt="Exchange" className="w-8 h-8 mx-auto" /> */}
+          <p className="mt-1  text-lg  font-extrabold text-ethYellow-600">Mela</p>
+        </div>
+        <div className="text-center text-[#85827d] w-1/5">
+          <Mine className="w-8 h-8 mx-auto" />
+          <p className="mt-1">Bank</p>
+        </div>
+        <div className="text-center text-[#85827d] w-1/5">
+          <Friends className="w-8 h-8 mx-auto" />
+          <p className="mt-1">Friends</p>
+        </div>
+        <div className="text-center text-[#85827d] w-1/5">
+          <Coins className="w-8 h-8 mx-auto" />
+          <p className="mt-1">Earn</p>
+        </div>
+        {/* <div className="text-center text-[#85827d] w-1/5">
+         <img src={hamsterCoin} alt="Airdrop" className="w-8 h-8 mx-auto" /> 
+          <p className="mt-1">Airdrop</p>
+        </div> */}
+      </div>
 </div>
 
     {/*   <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
--- app/protected/protectedlist/page.tsx
+++ app/protected/protectedlist/page.tsx
@@ -9,7 +9,7 @@ function Page() {
   return (
     <div className='bg-ethBlack-950  w-full h-full  mb-2 flex flex-col'>
    <BackButtonDemo/>   
-   <div className=" bg-ethBlack-950  p-8  grid grid-cols-3 gap-0 ">
+   <div className=" bg-ethBlack-950  p-12  grid grid-cols-3 gap-0 ">
       {/* Create an array with 100 elements and map over it */}
       {Array.from({ length: 100 }).map((_, index) => (
         <div className="px-4 mt-4 flex justify-center" key={index}>
@@ -25,7 +25,7 @@ function Page() {
         </div>
       ))}
     </div>
-    <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs p-4">
+    <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs p-2">
         <div className="text-center text-[#85827d] w-1/5 bg-[#1c1f24] m-1 p-2 rounded-2xl">
         {/*   <img src={binanceLogo} alt="Exchange" className="w-8 h-8 mx-auto" /> */}
           <p className="mt-1  text-lg  font-extrabold text-ethYellow-600">Mela</p>
