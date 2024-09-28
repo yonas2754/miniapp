fe4a10dc8d0018bbbd7f2abeae21a312983f4215
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-09-28 21:46:41 +0200

    first commit56

--- app/protected/protectedlist/page.tsx
+++ app/protected/protectedlist/page.tsx
@@ -6,40 +6,17 @@ import { BackButton, useShowPopup } from '@vkruglikov/react-telegram-web-app';
 const BackButtonDemo: FC = () => {
   const [buttonState, setButtonState] = useState<{
     show: boolean;
-  }>();
-  const showPopup = useShowPopup();
+  }>({show: true});
+ 
 
   return (
     <>
       <Typography.Title level={3}>BackButton</Typography.Title>
-      <Form
-        labelCol={{ span: 6 }}
-        name="BackButtonDemo"
-        layout="horizontal"
-        autoComplete="off"
-      >
-        <Form.Item>
-          <Button
-            block
-            type="primary"
-            onClick={() =>
-              setButtonState({
-                show: !buttonState?.show,
-              })
-            }
-          >
-            {buttonState?.show ? 'Hide BackButton' : 'Show BackButton'}
-          </Button>
-        </Form.Item>
-      </Form>
+     
       <div>
         {buttonState?.show && (
           <BackButton
-            onClick={() => {
-              showPopup({
-                message: 'back button click',
-              });
-            }}
+            onClick={() => window.history.back()}
           />
         )}
       </div>
