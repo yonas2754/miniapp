56ab1b04e8b256245239f33cbe7d77314f8e42d2
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-09-28 22:50:47 +0200

    first commit62

--- app/protected/protectedlist/page.tsx
+++ app/protected/protectedlist/page.tsx
@@ -6,27 +6,19 @@ function Page() {
   return (
     <div>
    <BackButtonDemo/>   
-   <div className=' bg-ethDeepBlue-950 w-full  h-full p-8'>
-    <div  className=''>
-    <div>
+   <div className=" bg-ethBlack-950 w-full h-full p-8 grid grid-cols-3 gap-4 ">
       {/* Create an array with 100 elements and map over it */}
       {Array.from({ length: 100 }).map((_, index) => (
-        <div className="px-4 mt-4 grid grid-cols-3 gap-1  overflow-scroll " key={index}>
-           <Link href="/protected/protectedlist">
+        <div className="px-4 mt-4 flex justify-center" key={index}>
           <div className="p-4 rounded-full circle-outer">
             <div className="w-full h-full rounded-full circle-inner">
               <p className='text-center font-bold text-lg text-ethYellow-600'>{index + 1}</p>
-             
+              <Link href="/protected/protectedlist"></Link>
             </div>
           </div>
-          </Link>
         </div>
       ))}
     </div>
-
-    </div>
-
-   </div>
     </div>
   )
 }
