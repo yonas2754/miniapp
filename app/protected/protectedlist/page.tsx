7cb3bfc713a1d552e50d5d2c627943841b8ab11e
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-09-28 21:27:37 +0200

    first commit55

--- app/protected/protectedlist/page.tsx
+++ app/protected/protectedlist/page.tsx
@@ -1,12 +1,49 @@
-import React from 'react'
-import { BackButton } from '@twa-dev/sdk/react';
+'use client'
+import { Button, Form, Typography } from 'antd';
+import { FC, useState } from 'react';
+import { BackButton, useShowPopup } from '@vkruglikov/react-telegram-web-app';
 
-function Page() {
+const BackButtonDemo: FC = () => {
+  const [buttonState, setButtonState] = useState<{
+    show: boolean;
+  }>();
+  const showPopup = useShowPopup();
+
   return (
-    <div className='h-screen w-full bg-red-400'>
-      <BackButton onClick={() => window.history.back()} />
-    </div>
-  )
-}
-
-export default Page
+    <>
+      <Typography.Title level={3}>BackButton</Typography.Title>
+      <Form
+        labelCol={{ span: 6 }}
+        name="BackButtonDemo"
+        layout="horizontal"
+        autoComplete="off"
+      >
+        <Form.Item>
+          <Button
+            block
+            type="primary"
+            onClick={() =>
+              setButtonState({
+                show: !buttonState?.show,
+              })
+            }
+          >
+            {buttonState?.show ? 'Hide BackButton' : 'Show BackButton'}
+          </Button>
+        </Form.Item>
+      </Form>
+      <div>
+        {buttonState?.show && (
+          <BackButton
+            onClick={() => {
+              showPopup({
+                message: 'back button click',
+              });
+            }}
+          />
+        )}
+      </div>
+    </>
+  );
+};
+export default BackButtonDemo;
