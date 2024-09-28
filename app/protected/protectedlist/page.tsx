c3fab3f641baf4b99e3022dc270f41048721049e
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-09-28 22:53:00 +0200

    first commit63

--- app/protected/protectedlist/page.tsx
+++ app/protected/protectedlist/page.tsx
@@ -6,7 +6,7 @@ function Page() {
   return (
     <div>
    <BackButtonDemo/>   
-   <div className=" bg-ethBlack-950 w-full h-full p-8 grid grid-cols-3 gap-4 ">
+   <div className=" bg-ethBlack-950 w-full h-full p-8 flex  gap-1 ">
       {/* Create an array with 100 elements and map over it */}
       {Array.from({ length: 100 }).map((_, index) => (
         <div className="px-4 mt-4 flex justify-center" key={index}>
