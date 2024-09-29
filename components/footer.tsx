c343efdd67a6c7864f7ee2b492610b998675492d
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-09-29 22:48:18 +0200

    first commit100

--- components/footer.tsx
+++ components/footer.tsx
@@ -12,25 +12,25 @@ function Footer() {
       <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs p-2">
       
         <div className={`text-center  w-1/5 ${pathname.includes('/protected')  ? ' bg-[#1c1f24] ' : ''}      m-1 p-2 rounded-2xl`}>
-        <Link href="/dashboard">
+        <Link href="/protected">
 
           <p className="mt-1 text-lg font-extrabold text-ethYellow-600">Mela</p>
           </Link>
         </div>
         <div className={`text-center text-[#85827d] w-1/5  ${pathname.includes('/protected')  ? ' bg-[#1c1f24] ' : ''} `}>
-        <Link href="/dashboard">
+        <Link href="/bank">
           <Mine className="w-8 h-8 mx-auto" />
           <p className="mt-1">Bank</p>
           </Link>
         </div>
         <div className={`text-center text-[#85827d] w-1/5  ${pathname.includes('/protected')  ? ' bg-[#1c1f24] ' : ''} `}>
-        <Link href="/dashboard">
+        <Link href="/friends">
           <Friends className="w-8 h-8 mx-auto" />
           <p className="mt-1">Friends</p>
           </Link>
         </div>
         <div className={`text-center text-[#85827d] w-1/5  ${pathname.includes('/protected')  ? ' bg-[#1c1f24] ' : ''} `}>
-        <Link href="/dashboard">
+        <Link href="/earn">
           <Coins className="w-8 h-8 mx-auto" />
           <p className="mt-1">Earn</p>
           </Link>
