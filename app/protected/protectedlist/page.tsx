30c5bb26d2137adcb310288487111371b98bf5cf
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-09-29 17:08:00 +0200

    first commit85

--- app/protected/protectedlist/page.tsx
+++ app/protected/protectedlist/page.tsx
@@ -42,7 +42,7 @@ const Page: React.FC = () => {
                         {ticketNumber}
                       </p>
                     </AlertDialogTrigger>
-                    <AlertDialogContent className='bg-ethGray-200 text-black fixed bottom-8 left-1/2 transform -translate-x-1/2 w-[90%] max-w-md rounded-xl shadow-lg'>
+                    <AlertDialogContent className='bg-ethGray-200 text-black fixed bottom-0  border-t-2  shadow-2xl shadow-ethYellow-600'>
                       <AlertDialogHeader>
                         <AlertDialogTitle>Mela Number #{ticketNumber}</AlertDialogTitle>
                         <AlertDialogDescription>
