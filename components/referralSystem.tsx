77ce1364b1b1818e189582b825aa25e9586c896b
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-09-30 15:51:04 +0300

    first commit109

--- components/referralSystem.tsx
+++ components/referralSystem.tsx
@@ -10,7 +10,7 @@ interface ReferralSystemProps {
 const ReferralSystem: React.FC<ReferralSystemProps> = ({ initData, userId, startParam }) => {
   const [referrals, setReferrals] = useState<string[]>([])
   const [referrer, setReferrer] = useState<string | null>(null)
-  const INVITE_URL = "https://t.me/referral_showcase_bot/start"
+  const INVITE_URL = "https://t.me/MelaOnTelegram_bot/MeleTelegram"
 
   useEffect(() => {
     const checkReferral = async () => {
