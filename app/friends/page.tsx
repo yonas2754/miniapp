a5d2976d01af3eea3042b23a35eb0fa8224fcd5e
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-09-30 15:52:55 +0300

    first commit110

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-8">Telegram Referral Demo</h1>
+      <h1 className="text-4xl font-bold mb-8">Mela invitation</h1>
       <ReferralSystem initData={initData} userId={userId} startParam={startParam} />
     </main>
   )
