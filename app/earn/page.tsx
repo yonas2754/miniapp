6463d418911b47504d7d77a8a8704075679cb4f9
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-10-04 17:14:09 +0300

    first commit134

--- app/earn/page.tsx
+++ app/earn/page.tsx
@@ -1,17 +1,21 @@
 'use client'
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+
 
 // Import Swiper styles
 import 'swiper/css';
 
-export default  function Home () {
+export default function Home() {
   return (
     <Swiper
       spaceBetween={50}
       slidesPerView={3}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
+      autoplay={{ delay: 3000 }} // Auto play slides every 3 seconds
+    
     >
       <SwiperSlide>Slide 1</SwiperSlide>
       <SwiperSlide>Slide 2</SwiperSlide>
