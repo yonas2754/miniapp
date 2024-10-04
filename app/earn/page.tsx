12670cb4070960c4c367ec3b0f3ed46635597403
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-10-04 23:17:46 +0300

    first commit138

--- app/earn/page.tsx
+++ app/earn/page.tsx
@@ -11,6 +11,9 @@ import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 
 export default function Page() {
+const ticket =[{'name':'yonas','ticketNum':4},{'name':'yonas2','ticketNum':1},{'name':'yonas2','ticketNum':2},
+  {'name':'yonas3','ticketNum':3},{'name':'yonas5','ticketNum':5},{'name':'yonas6','ticketNum':6},{'name':'yonas7','ticketNum':7},{'name':'yonas8','ticketNum':8},{'name':'yonas9','ticketNum':9},{'name':'yonas10','ticketNum':10},{'name':'yonas11','ticketNum':11},{'name':'yonas12','ticketNum':12}
+]
   return (
     <section className='py-12'>
       <div className='container'>
@@ -20,44 +23,21 @@ export default function Page() {
           autoplay={{ delay: 30, disableOnInteraction: false }} // Autoplay configuration
           modules={[ Autoplay]}
           onSwiper={swiper => console.log(swiper)}
-          className='h-96 w-full rounded-lg'
+          className='h-1/2 bg-ethLightBlue-400  w-full rounded-lg'
         >
+            {ticket.map((t, index) => (
+            <SwiperSlide key={index}>
+              <div className='flex flex-col h-full w-full items-center justify-center'>
+              <p className='circle-inner'>{t.ticketNum}</p>
+               <p>{t.name}</p>
+             
+
+              </div>
+            </SwiperSlide>
+            ))}
+                   
+        
          
-            <SwiperSlide >
-              <div className='flex h-full w-full items-center justify-center'>
-               <h1>yonas</h1>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide >
-              <div className='flex h-full w-full items-center justify-center'>
-               <h1>yonas2</h1>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide >
-              <div className='flex h-full w-full items-center justify-center'>
-               <h1>yonas3</h1>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide >
-              <div className='flex h-full w-full items-center justify-center'>
-               <h1>yonas4</h1>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide >
-              <div className='flex h-full w-full items-center justify-center'>
-               <h1>yonas5</h1>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide >
-              <div className='flex h-full w-full items-center justify-center'>
-               <h1>yonas6</h1>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide >
-              <div className='flex h-full w-full items-center justify-center'>
-               <h1>yonas7</h1>
-              </div>
-            </SwiperSlide>
           
         </Swiper>
       </div>
