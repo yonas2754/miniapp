6c8b0531b29c41e0a0f0a603e56d6da2e526b17b
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-10-04 16:44:57 +0300

    first commit132

--- app/bank/page.tsx
+++ app/bank/page.tsx
@@ -102,7 +102,7 @@ function BankForm({
           {isSelected && <span className="ml-2 text-lg">✔️</span>}
         </Button>
       </AlertDialogTrigger>
-      <AlertDialogContent className="bg-ethGray-200 mx-auto mb-4 ">
+      <AlertDialogContent className="bg-ethGray-200 mx-auto mt-[20%]">
         <AlertDialogHeader>
           <AlertDialogTitle>Fill Your Name and Account Number for {bankName}</AlertDialogTitle>
           <AlertDialogDescription>
--- app/earn/page.tsx
+++ app/earn/page.tsx
@@ -1,10 +1,16 @@
 import Footer from '@/components/footer'
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
 
 function Page() {
   return (
     <div>
-      <h1>Earn</h1>
+      <Swiper autoplay={{ delay: 3000 }} loop>
+  <SwiperSlide>Slide 1</SwiperSlide>
+  <SwiperSlide>Slide 2</SwiperSlide>
+  <SwiperSlide>Slide 3</SwiperSlide>
+</Swiper>
       
       <Footer/>
     </div>
