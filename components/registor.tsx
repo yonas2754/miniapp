73f8048fffaaea72f97bf748ae16c5b7c96c00ea
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-10-15 16:25:47 +0300

    first commit176

--- components/registor.tsx
+++ components/registor.tsx
@@ -83,11 +83,11 @@ console.log("data"+data)
         
 
    
-    
+  
   }
 
   return (
-     <div className=" fixed top-0  left-0 w-full h-screen  z-40 bg-ethBlack-600">
+     <div className=" fixed top-0  left-0 w-full h-screen  z-40 bg-ethBlack-600  flex  justify-center items-center">
     
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className=" flex flex-1 flex-col min-w-2/3  w-full space-y-6">
