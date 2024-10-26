b96a4a24e6846d6a46a6a8f1de64b3865fe9bcbe
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-10-26 04:37:00 +0300

    first commit217

--- app/earn/page.tsx
+++ app/earn/page.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import History from "./history"
 import {
   Tabs,
   TabsContent,
@@ -90,7 +91,7 @@ export default function TabsDemo() {
           </CardHeader>
           <CardContent className="space-y-2">
      
-          <HistoryForm chatId="7277258087"/>
+          <History chatId="7277258087"/>
           
           </CardContent>
          
