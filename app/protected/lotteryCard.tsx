d7f2d2da92933b6c73ed73369751b9caca58da6f
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-10-27 00:21:34 +0300

    first commit226

--- app/protected/lotteryCard.tsx
+++ app/protected/lotteryCard.tsx
@@ -1,10 +1,17 @@
 import React, { useMemo } from 'react';
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card"; 
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 import Image from 'next/image';
 import { emojiSet } from '@/lib/Constants';
 
 // Utility function to get a random emoji from the expanded emojiSet
-const getRandomEmoji = (x:number): string => emojiSet[x];
+const getRandomEmoji = (x: number): string => emojiSet[x];
 
 interface LotteryCardProps {
   date: string;
@@ -15,27 +22,26 @@ interface LotteryCardProps {
 
 const LotteryCard: React.FC<LotteryCardProps> = ({ date, amount, price, initialEmoji }) => {
   // Memoize the random emoji selection to ensure it doesn't change
-  const emoji = getRandomEmoji(initialEmoji)
+  const emoji = getRandomEmoji(initialEmoji);
 
   return (
-    <Card className="bg-ethLightBlue-950 text-white">
-      <CardHeader>
-        <CardTitle>Day at {date}</CardTitle>
-        <CardDescription>
-          {/* Display a large, constant random emoji */}
-          <div className="flex justify-center my-4 text-6xl">
+    <Card className="bg-gradient-to-r from-blue-500 to-blue-700 text-white shadow-lg transition-transform transform hover:scale-105 hover:shadow-xl duration-300">
+      <CardHeader className="p-6 text-center">
+        <CardTitle className="text-2xl font-bold">{`Day at ${date}`}</CardTitle>
+        <CardDescription className="mt-2">
+          <div className="flex justify-center my-4 text-6xl animate-bounce">
             {emoji}
           </div>
         </CardDescription>
       </CardHeader>
-      <CardContent className="space-y-2">
-        <div className="flex items-center justify-center space-x-1">
-          <Image src="/image/lottery_winning3.png" alt="Dollar Icon" width={24} height={24} />
-          <p className="text-lg">{amount}</p>
+      <CardContent className="p-4 space-y-4">
+        <div className="flex items-center justify-center space-x-2">
+          <Image src="/image/lottery_winning3.png" alt="Dollar Icon" width={32} height={32} />
+          <p className="text-xl font-semibold">{amount}</p>
         </div>
       </CardContent>
-      <CardFooter>
-        <p className="text-sm">Price: {price}</p>
+      <CardFooter className="flex justify-between p-4 bg-opacity-50 bg-black rounded-b-lg">
+        <p className="text-sm">Price: <span className="font-bold">{price}</span></p>
       </CardFooter>
     </Card>
   );
