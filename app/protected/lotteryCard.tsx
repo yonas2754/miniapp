9630d0a01e17cfc8fe688f563fe77baf182869ed
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-10-27 00:44:14 +0300

    first commit231

--- app/protected/lotteryCard.tsx
+++ app/protected/lotteryCard.tsx
@@ -26,15 +26,15 @@ const LotteryCard: React.FC<LotteryCardProps> = ({ date, amount, price, initialE
 
   return (
     <Card className="bg-gradient-to-r from-ethLightBlue-700 to-ethLightBlue-900 text-white shadow-lg transition-transform transform hover:scale-105 hover:shadow-xl duration-300">
-      <CardHeader className="p-6 text-center">
-        <CardTitle className="text-2xl font-bold">{`Day at ${date}`}</CardTitle>
+      <CardHeader className="p-2 text-center">
+        <CardTitle className="text-2xl font-bold">{`At ${date}`}</CardTitle>
         <CardDescription className="mt-2">
           <div className="flex justify-center my-4 text-6xl animate-bounce">
             {emoji}
           </div>
         </CardDescription>
       </CardHeader>
-      <CardContent className="p-4 space-y-4">
+      <CardContent className="p-2 space-y-4">
         <div className="flex items-center justify-center space-x-2">
           <Image src="/image/lottery_winning3.png" alt="Dollar Icon" width={32} height={32} />
           <p className="text-xl font-semibold">{amount}</p>
