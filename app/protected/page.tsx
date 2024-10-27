766d8d53f56ed39873e9d17357fbb037a8e377c9
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-10-27 22:03:26 +0300

    first commit257

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -5,6 +5,7 @@ import { Backend_URL } from '@/lib/Constants';
 import { getSession } from '@/utils/session';
 import Link from 'next/link';
 import LotteryCard from './lotteryCard';
+import Footer from '@/components/footer';
 type ele ={
     id: string;
     gameType: string;
@@ -33,6 +34,7 @@ async function page() {
     
 
   return (
+    <div>
     <div className='bg-ethBlack-600 text-white w-full h-full min-h-screen font-bold flex flex-col max-w-xl'>
           <Posts chatId="7277258087" />
           <div className='flex flex-col rounded-t-[48px] bg-ethBlack-500 border-t-2 shadow-2xl border-ethYellow-500 w-full grow p-8'>
@@ -42,7 +44,7 @@ async function page() {
     <TabsTrigger value="tacket">Mela Tacket</TabsTrigger>
     <TabsTrigger value="winner">Mela Winner</TabsTrigger>
   </TabsList>
-  <TabsContent value="my" className="pt-4 grid grid-cols-2 gap-2">
+  <TabsContent value="my" className="pt-4 grid grid-cols-2 gap-4">
 
     {activeGames.map((element:ele) => (
         <div key={element.id}>
@@ -61,6 +63,8 @@ async function page() {
   </TabsContent>
 </Tabs>
 </div>
+    </div>
+    <Footer/>
     </div>
   )
 }
