f72c0b78ded89d0795c79eeaad995a9bda7977c2
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-10-28 19:38:28 +0300

    first commit258

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -81,7 +81,7 @@ async function page() {
   {userGames?.profiles?.map((profile:Profile) => (
         <div key={profile.id}>
            
-          
+          {JSON.stringify(profile)}
     </div>))}
 
   </TabsContent>
