47d2067c41108050200661861a38a609831b4bcd
yonasethiotelecom <149766574+yonasethiotelecom@users.noreply.github.com>
2024-10-29 16:53:55 +0300

    first commit259

--- components/profileForm.tsx
+++ components/profileForm.tsx
@@ -160,7 +160,7 @@ export default function ProfileForm() {
                       mode="single"
                       selected={field.value}
                       onSelect={field.onChange}
-                      disabled={(date) => date < new Date()}
+                      disabled={(date) => date <= new Date()}
                       initialFocus
                     />
                   </PopoverContent>
@@ -213,7 +213,7 @@ export default function ProfileForm() {
                       mode="single"
                       selected={field.value}
                       onSelect={field.onChange}
-                      disabled={(date) => date < new Date()}
+                      disabled={(date) => date <= new Date()}
                       initialFocus
                     />
                   </PopoverContent>
