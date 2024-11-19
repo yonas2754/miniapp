414e1467a3ae0c4cddba69b6eda177af8e81e138
Yonas Asfaw <yonasmechale@gmail.com>
2024-11-19 11:47:42 +0300

    it best start

--- app/bank/page.tsx
+++ app/bank/page.tsx
@@ -1,14 +1,31 @@
 import Footer from '@/components/footer'
 import ProfileForm from '@/components/profileForm'
 import React from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
 
 function page() {
   return (
     <div>
-    <div className=' bg-ethLightBlue-950 w-full flex justify-center items-center  h-full '>
+
+
+<Tabs defaultValue="account" className="w-[400px]">
+  <TabsList>
+    <TabsTrigger value="create">create</TabsTrigger>
+    <TabsTrigger value="approve">approve</TabsTrigger>
+  </TabsList>
+  <TabsContent value="create">
+  <div className=' bg-ethLightBlue-950 w-full flex justify-center items-center  h-full '>
       <ProfileForm/>
-      
     </div>
+    </TabsContent>
+  <TabsContent value="approve">
+    Change your approve here.</TabsContent>
+</Tabs>
+
+
+
+  
     <Footer/></div>
   )
 }
